3f606f9d1964293cfb87edc74ba86f3099a82066
Serge Dukic <serge.dukic@clearpoint.co.nz>
2016-11-30 22:48:11 +1300

    Added for loop
    Grab all the obselete alarms and then loop over them, deleting each one.
    
    Issue(s): None

--- clean_old_alarms.sh
+++ clean_old_alarms.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
-aws cloudwatch describe-alarms --state-value INSUFFICIENT_DATA | jq -r '.MetricAlarms[].AlarmName'
+echo "Starting job to delete INSUFFICIENT_DATA CloudWatch alarms..."
+
+for obseleteAlarm in $(aws cloudwatch describe-alarms --state-value INSUFFICIENT_DATA | jq -r '.MetricAlarms[].AlarmName')
+do
+  echo "Deleting alarm with name: $obseleteAlarm"
+  aws cloudwatch delete-alarms --alarm-name $obseleteAlarm
+done
