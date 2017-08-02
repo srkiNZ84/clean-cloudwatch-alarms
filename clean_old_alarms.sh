3a1cd37a032062867d28a9c3e3939b6b85c3e38f
Serge Dukic <serge.dukic@clearpoint.co.nz>
2017-08-03 11:41:23 +1200

    Ensure output is in JSON format
    Make sure that we explictly specify that the output should be in JSON format as we're passing it to jq.
    
    Issue(s): None

--- clean_old_alarms.sh
+++ clean_old_alarms.sh
@@ -2,7 +2,7 @@
 
 echo "Starting job to delete INSUFFICIENT_DATA CloudWatch alarms..."
 
-for obseleteAlarm in $(aws cloudwatch describe-alarms --state-value INSUFFICIENT_DATA | jq -r '.MetricAlarms[].AlarmName')
+for obseleteAlarm in $(aws --output json cloudwatch describe-alarms --state-value INSUFFICIENT_DATA | jq -r '.MetricAlarms[].AlarmName')
 do
   echo "Deleting alarm with name: $obseleteAlarm"
   aws cloudwatch delete-alarms --alarm-name $obseleteAlarm
